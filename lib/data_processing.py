9d1c35add57dbedf4e813cf1608bbcccc5f2e6a7
Michelangelo Lopes <michelangelopes@yahoo.com.br>
2022-03-14 03:06:44 -0300

    adicionando pequenas alterações para utilizar as novas estruturas de dados definidas, correção de alguns erros e adição de função para anonimizar os dados principais

--- lib/data_processing.py
+++ lib/data_processing.py
@@ -27,8 +27,7 @@ def get_room_mapping_from_excel_file(filepath, worksheet_name):
         if classroom not in room_mapping[day][hour]:
             room_mapping[day][hour][classroom] = []
         
-        component_list = [component_code, component_class]
-        room_mapping[day][hour][classroom].append(component_list)
+        room_mapping[day][hour][classroom] = [component_code, component_class]
 
     return room_mapping
 
@@ -37,8 +36,11 @@ def get_academics_mapping_from_pickle_file(filepath):
 
     academics_mapping = {}
 
-    students = {}
-    professors = {}
+    students = []
+    professors = []
+
+    professors_ids = []
+    students_ids = []
 
     for infos in data['data']:
         infos_subject = infos[0] # duas colunas, contendo as informações da disciplina; primeira com os nomes das infos e segunda com: disciplina, turma, nome do docente
@@ -53,7 +55,7 @@ def get_academics_mapping_from_pickle_file(filepath):
 
         infos_students = infos[1] # informações dos alunos: matrícula, nome, curso
 
-        students_ids = infos_students['Matrícula'].tolist() # lista com as matrículas
+        students_registration_numbers = infos_students['Matrícula'].tolist() # lista com as matrículas
         students_courses = infos_students['Curso'].tolist() # lista com os cursos
 
         if component_code not in academics_mapping: # adiciona o código da disciplina apenas quando aparece pela primeira vez
@@ -62,88 +64,84 @@ def get_academics_mapping_from_pickle_file(filepath):
         if component_class not in academics_mapping[component_code]: # os estudantes em uma turma de um determinado código de disciplina são os mesmos, então só adicionamos na primeira vez que aparecem
             academics_mapping[component_code][component_class] = []
 
-            if professor_name not in professors:
-                component_professor = classes.Professor(professor_name)
-                professors[professor_name] = component_professor
+            if professor_name not in professors_ids:
+                component_professor = classes.Professor(name = professor_name)
+                professors_ids.append(professor_name)
+                professors.append(component_professor)
             else:
-                component_professor = professors[professor_name]
+                index = professors_ids.index(professor_name)
+                component_professor = professors[index]
 
             academics_mapping[component_code][component_class].append(component_professor)
 
-            for i in range(0, len(students_ids)):
-                student_id = str(students_ids[i])
+            for i in range(0, len(students_registration_numbers)):
+                student_registration_number = str(students_registration_numbers[i])
                 student_course = students_courses[i]
-                new_student = classes.Student(student_id, student_course)
+                student_course = student_course.replace("- NOVA IGUAÇU", "").split("- Presencial")[0].split("-")
+                student_course = " - ".join([string.strip() for string in student_course])
 
-                if student_id not in students:
-                    students[student_id] = new_student
-                    academics_mapping[component_code][component_class].append(new_student)
+                if student_registration_number not in students_ids:
+                    student = classes.Student(registration_number = student_registration_number, course = student_course)
+                    students_ids.append(student_registration_number)
+                    students.append(student)
                 else:
-                    academics_mapping[component_code][component_class].append(students[student_id])            
+                    index = students_ids.index(student_registration_number)
+                    student = students[index]
     
+                academics_mapping[component_code][component_class].append(student)
+
     return academics_mapping, students, professors
 
 def remove_not_imported_component_code(room_mapping, academics_mapping):
+    remove = []
     for day in room_mapping:
         for hour in room_mapping[day]:
             for classroom in room_mapping[day][hour]:
-                indexes_to_remove = []
-                count = 0
-                for component_code, _ in room_mapping[day][hour][classroom]:
-                    if component_code not in academics_mapping:
-                        indexes_to_remove.append(count)
-                    count += 1
-                count = 0
-                for remove_index in indexes_to_remove:
-                    del room_mapping[day][hour][classroom][remove_index - count]
-                    count += 1
+                component_code, _ = room_mapping[day][hour][classroom]
+                if component_code not in academics_mapping:
+                    remove.append([day, hour, classroom])
+    
+    for day, hour, classroom in remove:                
+        del room_mapping[day][hour][classroom]
 
 def remove_not_imported_component_class(room_mapping, academics_mapping):
+    remove = []
     for day in room_mapping:
         for hour in room_mapping[day]:
             for classroom in room_mapping[day][hour]:
-                indexes_to_remove = []
-                count = 0
-                for component_code, component_class in room_mapping[day][hour][classroom]:
-                    if component_class not in academics_mapping[component_code]:
-                        indexes_to_remove.append(count)
-                    count += 1
-                count = 0
-                for remove_index in indexes_to_remove:
-                    del room_mapping[day][hour][classroom][remove_index - count]
-                    count += 1
+                component_code, component_class = room_mapping[day][hour][classroom]
+                if component_class not in academics_mapping[component_code]:
+                    remove.append([day, hour, classroom])
+
+    for day, hour, classroom in remove:                
+        del room_mapping[day][hour][classroom]
 
 def fill_empty_component_class(room_mapping, academics_mapping):
-    auxiliar = {}
+    classes_to_fix = {}
 
     for component_code in academics_mapping:
-        auxiliar[component_code] = list(academics_mapping[component_code].keys())
+        classes_to_fix[component_code] = list(academics_mapping[component_code].keys()) # pega todas as turmas de uma disciplina importadas do sigaa
 
-    for _ in range(0, 2):
+    for _ in range(0, 3): # normalmente uma disciplina tem duas turmas, então fazemos três iterações para garantir que as modificações sejam completas
         values_to_remove = []
         for day in room_mapping:
             for hour in room_mapping[day]:
                 for classroom in room_mapping[day][hour]:
-                    count = 0
-                    tuples_to_modify = []
-                    for component_code, component_class in room_mapping[day][hour][classroom]:
-                        imported_classes = auxiliar[component_code]
-                        classes_count = len(imported_classes)
-                        if component_class == None:
-                            if classes_count == 1:
-                                tuples_to_modify.append((count, imported_classes[0]))
-                                if (component_code, imported_classes[0]) not in values_to_remove:
-                                    values_to_remove.append((component_code, imported_classes[0]))
-                        else:
-                            if component_class in auxiliar[component_code]:
-                                if (component_code, component_class) not in values_to_remove:
-                                    values_to_remove.append((component_code, component_class))
-                        count += 1
-                    
-                    for modify_index, new_component_class in tuples_to_modify:
-                        room_mapping[day][hour][classroom][modify_index][1] = new_component_class 
+                    component_code, component_class = room_mapping[day][hour][classroom]
+                    classes_count = len(classes_to_fix[component_code]) # quantidade de turmas de uma disciplina
+                    if component_class == None:
+                        if classes_count == 1:
+                            # se não tem turma especificada no pdf, mas tem no sigaa, usa a do sigaa
+                            room_mapping[day][hour][classroom][1] = classes_to_fix[component_code][0]
+                            if (component_code, classes_to_fix[component_code][0]) not in values_to_remove:
+                                values_to_remove.append((component_code, classes_to_fix[component_code][0])) # anota que essa turma da disciplina já está correta
+                    else:
+                        if component_class in classes_to_fix[component_code]:
+                            if (component_code, component_class) not in values_to_remove:
+                                values_to_remove.append((component_code, component_class)) # anota que essa turma da disciplina já está correta
+
         for component_code, component_class in values_to_remove:
-            auxiliar[component_code].remove(component_class)
+            classes_to_fix[component_code].remove(component_class) # remove as classes corrigidas
 
 def get_classrooms_data_from_excel_file(filepath):
     # a função carrega dados de uma planilha; após isso, cria e retorna um dicionário da forma
@@ -151,19 +149,37 @@ def get_classrooms_data_from_excel_file(filepath):
 
     excel_workbook = load_workbook(filepath)
     worksheet = excel_workbook.active
-    classrooms = {}
+    classrooms = []
 
     for i in range(2, worksheet.max_row + 1):
-        classroom = worksheet.cell(row = i, column = 1).value
+        id = worksheet.cell(row = i, column = 1).value
         height = worksheet.cell(row = i, column = 2).value
         width = worksheet.cell(row = i, column = 3).value
         length = worksheet.cell(row = i, column = 4).value
 
-        classrooms[classroom] = classes.Classroom(height, width, length)
+        classrooms.append(classes.Classroom(id, height, width, length))
 
     return classrooms
 
-def get_and_save_data(filename):
+def get_academics_dict(professors, students):
+    academics = {}
+    id_count = 1
+
+    for professor in professors:
+        id = "ACADÊMICO_" + str(id_count)
+        academics[id] = (professor.id, "")
+        professor.academic_id = id
+        id_count += 1
+
+    for student in students:
+        id = "ACADÊMICO_" + str(id_count)
+        academics[id] = ("", student.id)
+        student.academic_id = id
+        id_count += 1
+    
+    return academics
+
+def get_processed_data(filename):
     xlsx_filepath_1, worksheet_name, pickle_filepath, xlsx_filepath_2 = file_operations.load_parameters_from_txt_file(filename)
     
     room_mapping = get_room_mapping_from_excel_file(xlsx_filepath_1, worksheet_name)
@@ -173,8 +189,68 @@ def get_and_save_data(filename):
     remove_not_imported_component_code(room_mapping, academics_mapping)
     fill_empty_component_class(room_mapping, academics_mapping)
     remove_not_imported_component_class(room_mapping, academics_mapping)
-    
-    file_operations.save_room_mapping_to_csv_file(room_mapping)
-    file_operations.save_academics_mapping_to_csv_file(academics_mapping)
 
-    return room_mapping, academics_mapping, students, professors, classrooms
+    academics = anonymize_data(room_mapping, academics_mapping, professors, students)
+
+    return room_mapping, academics_mapping, academics, professors, students, classrooms
+
+def anonymize_dict(dict, prefix):
+    id_count = 1
+    dict_keys = list(dict.keys())
+    while dict_keys != []:
+        new_key = prefix + "_" + str(id_count)
+        dict[new_key] = dict.pop(dict_keys.pop(0))
+
+        id_count += 1
+
+def anonymize_list(list, prefix):
+    id_count = 1
+
+    for element in list:
+        id = prefix + "_" + str(id_count)
+        element.id = id
+        id_count += 1
+
+def anonymize_data(room_mapping, academics_mapping, professors, students):
+    anonymize_list(professors, "PROFESSOR")
+    anonymize_list(students, "ALUNO")
+    academics = get_academics_dict(professors, students)
+
+    # dicionário com key = old_key; value = old_key
+    component_codes = {}
+    
+    for component_code in list(academics_mapping.keys()):
+        component_codes[component_code] = component_code
+
+    # dicionário com key = new_key; value = old_key
+    anonymize_dict(component_codes, "DISCIPLINA")
+    anonymize_dict(academics_mapping, "DISCIPLINA")
+
+    for day in room_mapping:
+        for hour in room_mapping[day]:
+            for classroom in room_mapping[day][hour]:
+                old_component_code, _ = room_mapping[day][hour][classroom]
+                for new_component_code in component_codes:
+                    if component_codes[new_component_code] == old_component_code:
+                        room_mapping[day][hour][classroom][0] = new_component_code
+                        break
+
+    return academics
+
+def find_class_instance_by_id(list, id):
+    for element in list:
+        if id == element.id:
+            return element
+    
+    return None
+
+def find_class_instance_by_academic_id(academics, professors, students, academic_id):
+    professor_id, student_id = academics[academic_id]
+
+    if professor_id == "":
+        academic = find_class_instance_by_id(students, student_id)
+
+    elif student_id == "":
+        academic = find_class_instance_by_id(professors, professor_id)
+
+    return academic
