a560ff0e5db157f9e16cf00f03813fb424912c2d
Michelangelo Lopes <michelangelopes@yahoo.com.br>
2022-03-14 03:09:09 -0300

    realização de alterações leves na main para se adequar as modificações nos outros arquivos e geração de csvs com dados anonimizados para disponibilizar no github

--- main.py
+++ main.py
@@ -47,12 +47,10 @@ def get_days_to_analyse(informed_day):
 
     return week_days
 
-def tracking(informed_academic, informed_id, informed_day, room_mapping, academics_mapping, students, professors, classrooms):
-    if informed_academic == "P":
-        choosed_academic = professors[informed_id]
-    elif informed_academic == "A":
-        choosed_academic = students[informed_id]
+def tracking(informed_id, informed_day, room_mapping, academics_mapping, academics, professors, students, classrooms):
+    choosed_academic = data_processing.find_class_instance_by_academic_id(academics, professors, students, informed_id)
 
+    print(choosed_academic.academic_id)
     # assumindo que o acadêmico fez o teste no primeiro dia que sentiu os sintomas da covid e informou que estava contaminado
     # ignorando dias de fim de semana por enquanto
     
@@ -62,14 +60,14 @@ def tracking(informed_academic, informed_id, informed_day, room_mapping, academi
     choosed_academic.infected_days = 0
     choosed_academic.infection_probability = 0
 
-    for student_id in students:
-        students[student_id].breathing_rate = 0.5
-    for professor_name in professors:
-        professors[professor_name].breathing_rate = 0.5
+    for student in students:
+        student.breathing_rate = random.uniform(0.25, 0.75)
+    for professor in professors:
+        professor.breathing_rate = random.uniform(0.75, 1)
 
     # quatro dias anteriores a notificação de contaminação, ignorando fim de semana
     days = get_days_to_analyse(informed_day)
-
+    
     #sábado e domingo
     # for student_id in students:
     #     students[student_id].infection_probability = random.uniform(0, 1)
@@ -84,36 +82,38 @@ def tracking(informed_academic, informed_id, informed_day, room_mapping, academi
     for day in days:
         for hour in room_mapping[day]:
             for classroom in room_mapping[day][hour]:
-                for component_code, component_class in room_mapping[day][hour][classroom]:
-                    # print(academics_mapping[component_code])
-                    # print(academics_mapping[component_code][component_class])
-                    # input()
-                    # print(choosed_academic in academics_mapping[component_code][component_class])
-                    if choosed_academic in academics_mapping[component_code][component_class]:
-                        print(day, hour, classroom, component_code, component_class)
-                        count_people = len(academics_mapping[component_code][component_class])
-                        count_infected = 0
-                        exhaled_room_volume_per_hour = 0
-                        room_volume = classrooms[classroom].volume
+                # print(day, hour, classroom)
+                component_code, component_class = room_mapping[day][hour][classroom]
+                # print(academics_mapping[component_code])
+                # print(academics_mapping[component_code][component_class])
+                # input()
+                # print(choosed_academic in academics_mapping[component_code][component_class])
+                if choosed_academic in academics_mapping[component_code][component_class]:
+                    print(day, hour, classroom, component_code, component_class)
+                    current_classroom = data_processing.find_class_instance_by_id(classrooms, classroom)
+                    count_people = len(academics_mapping[component_code][component_class])
+                    count_infected = 0
+                    exhaled_room_volume_per_hour = 0
+                    room_volume = current_classroom.volume
 
-                        for academic in academics_mapping[component_code][component_class]:
-                            # print(academic)
-                            exhaled_room_volume_per_hour += academic.breathing_rate
-                            if academic not in affected_academics and academic != choosed_academic:
-                                affected_academics.append(academic)
-                            if academic.has_covid and academic.can_transmit:
-                                count_infected += 1
-                        
-                        print(count_people, count_infected, exhaled_room_volume_per_hour, room_volume)
-                        classroom_contamination_prob = rudnick(count_people, count_infected, exhaled_room_volume_per_hour=exhaled_room_volume_per_hour, room_volume=room_volume)
-                        print(classroom_contamination_prob)
+                    for academic in academics_mapping[component_code][component_class]:
+                        # print(academic)
+                        exhaled_room_volume_per_hour += academic.breathing_rate
+                        if academic not in affected_academics and academic != choosed_academic:
+                            affected_academics.append(academic)
+                        if academic.has_covid and academic.can_transmit:
+                            count_infected += 1
+                    
+                    print(count_people, count_infected, exhaled_room_volume_per_hour, room_volume)
+                    classroom_contamination_prob = rudnick(count_people, count_infected, exhaled_room_volume_per_hour=exhaled_room_volume_per_hour, room_volume=room_volume)
+                    print(classroom_contamination_prob)
 
-                        for academic in academics_mapping[component_code][component_class]:
-                            # analisar como "juntar" probabilidades
-                            # teste com somatório ficou esquisito
-                            if not academic.has_covid:
-                                academic.infection_probability = classroom_contamination_prob * (1 + academic.infection_probability)
-                                # += classroom_contamination_prob 
+                    for academic in academics_mapping[component_code][component_class]:
+                        # analisar como "juntar" probabilidades
+                        # teste com somatório ficou esquisito
+                        if not academic.has_covid:
+                            academic.infection_probability = classroom_contamination_prob * (1 + academic.infection_probability)
+                            # += classroom_contamination_prob 
 
     calculate_contamination_associated_values(professors)
     calculate_contamination_associated_values(students)
@@ -121,53 +121,58 @@ def tracking(informed_academic, informed_id, informed_day, room_mapping, academi
     # print(affected_academics)
     # print(academics_mapping)
 
-    for student_id in students:
-        if students[student_id] in affected_academics:
-            if students[student_id].has_covid:
-                print(student_id, students[student_id].infection_probability, "foi contaminado")
-            else:
-                print(student_id, students[student_id].infection_probability, "não foi contaminado")
+    for academic in affected_academics:
+        if academic.has_covid:
+            print(academic.academic_id, academic.id, academic.infection_probability, "foi contaminado")
+        else:
+            print(academic.academic_id, academic.id, academic.infection_probability, "não foi contaminado")
     
-    for professor_name in professors:
-        if professors[professor_name] in affected_academics:
-            if professors[professor_name].has_covid:
-                print(professor_name, professors[professor_name].infection_probability, "foi contaminado")
+def calculate_contamination_associated_values(academics_instances):
+    for academic_instance in academics_instances:
+        if academic_instance.has_covid:
+            if academic_instance.infected_days == 14:
+                academic_instance.has_covid = False
+                academic_instance.can_transmit = False
+                # academic_instance.has_symptoms = False
+                academic_instance.infected_days = 0
+                academic_instance.infection_probability = 0
             else:
-                print(professor_name, professors[professor_name].infection_probability, "não foi contaminado")
-
-def calculate_contamination_associated_values(academics):
-    for academic_id in academics:
-        if academics[academic_id].has_covid:
-            if academics[academic_id].infected_days == 14:
-                academics[academic_id].has_covid = False
-                academics[academic_id].can_transmit = False
-                academics[academic_id].has_symptoms = False
-                academics[academic_id].infected_days = 0
-                academics[academic_id].infection_probability = 0
-            else:
-                academics[academic_id].infected_days += 1
+                academic_instance.infected_days += 1
         else:
             random_probability = random.uniform(0.5, 1)
 
-            if academics[academic_id].infection_probability > random_probability:
-                academics[academic_id].has_covid = True
-                academics[academic_id].can_transmit = True
-                academics[academic_id].has_symptoms = False
-                academics[academic_id].infected_days = 0
-
+            if academic_instance.infection_probability > random_probability:
+                academic_instance.has_covid = True
+                academic_instance.can_transmit = True
+                # academic_instance.has_symptoms = False
+                academic_instance.infected_days = 0
 
 def main():
     # (opt, args) = parse_terminal_options()
 
-    room_mapping, academics_mapping, students, professors, classrooms = data_processing.get_and_save_data("lib/file.txt")
-    informed_academic, informed_id, informed_day = file_operations.load_parameters_from_txt_file("lib/info.txt")
+    # room_mapping, academics_mapping, academics, professors, students, classrooms = file_operations.load_anonymized_data_from_csv_files()
+    room_mapping, academics_mapping, academics, professors, students, classrooms = data_processing.get_processed_data("lib/file.txt")
+    # file_operations.save_structures_to_csv_files(room_mapping, academics_mapping, academics, professors, students, classrooms)
+    informed_id, informed_day = file_operations.load_parameters_from_txt_file("lib/info.txt")
     # informed_academic = input("Digite \"P\" se for Professor ou \"A\" se for Aluno: ")
     # informed_id = input("Digite a sua matrícula (aluno) ou nome completo (professor): ")
     # informed_day = input("Digite a data na qual você descobriu estar com Covid-19: ")
-
     # print(classrooms)
-    
-    tracking(informed_academic, informed_id, informed_day, room_mapping, academics_mapping, students, professors, classrooms)
+    # print(room_mapping)
+    # input()
+    # print(academics_mapping)
+    # input()
+    # print(academics)
+    # input()
+    # print(professors)
+    # input()
+    # print(students)
+    # input()
+    # print(classrooms)
+    # input()
+    # for professor_name in professors:
+    #     print(professors[professor_name].name)
+    tracking(informed_id, informed_day, room_mapping, academics_mapping, academics, professors, students, classrooms)
 
     return
 
